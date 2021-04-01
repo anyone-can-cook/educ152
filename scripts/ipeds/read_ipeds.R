1df6fe327a90ece62756a1164cf1f69058245fac
cyouh95 <25449416+cyouh95@users.noreply.github.com>
2021-03-31 20:21:09 -0700

    label ipeds data

--- scripts/ipeds/read_ipeds.R
+++ scripts/ipeds/read_ipeds.R
@@ -36,6 +36,8 @@ stata_dir <- file.path(input_dir, 'stata_files')
   #stata_dir
   #list.files(path = stata_dir)
 
+output_dir <- file.path(ipeds_dir, 'output_data')
+
 ## -----------------------------------------------------------------------------
 ## function to download ipeds files
 ## -----------------------------------------------------------------------------
@@ -99,7 +101,7 @@ years <- c('2015','2016','2017','2018','2019')
 # loop for data year
 for (y in 1:length(years)) { 
   
-  writeLines(str_c("\n i=", i, "; year=", years[[y]]))
+  writeLines(str_c("\n y=", y, "; year=", years[[y]]))
   
   # loop for file_types c('csv','dict','stata')
   for (i in 1:length(file_types)) { 
@@ -122,20 +124,120 @@ for (y in 1:length(years)) {
   }
 }
 
+## -----------------------------------------------------------------------------
+## functions to label ipeds files
+## -----------------------------------------------------------------------------
 
-# STUFF FOR CRYSTAL TO DO:
+# Function to read in CSV data (default is use revised version if available)
+csv_to_df <- function(dir_name, file_name, year, rv = TRUE) {
+  if (rv && file.exists(file.path(dir_name, str_c(file_name, '_rv', '.csv')))) {
+    file_name <- str_c(file_name, '_rv')
+  }
+  
+  writeLines(file.path(dir_name, str_c(file_name, '.csv')))
+  df <- read_csv(file = file.path(dir_name, str_c(file_name, '.csv')), na = c('.'))
+  names(df) <- str_to_lower(names(df))
+  
+  df <- df %>% add_column(year = year, .before = 1)
+  
+  df
+}
 
-  # READ CSV DATA INTO R
-  # LABEL ALL VARIABLES AND ADD VALUE LABELS TO VARIABLES WITH VALUE LABELS
-    # YOU DECIDE WHETHER YOU WANT TO USE THE XLSX DICTIONARIES OR STATA DO FILES AS INPUTS TO VARIABLE/VALUE LABELS
-  # FOR EACH DIFFERENT KIND OF DATA (E.G., flags; hd; educational offerings...; student charges) append the datasets from different years to create a panel
-    # create a year identifier to identify year
-    # this will involve ovewriting variable/value labels
-    # save these r datasets to local machine 
-      # C:\Users\ozanj\Documents\educ152\data\ipeds\output_data
-  # merge the following panels by unitid and year:
-      # hd, flags, academic year charges
-      # save R dataset to local machine
+# Function to get labels from stata file
+get_stata_labels <- function(dir_name, file_name) {
+  stata <- readLines(file.path(dir_name, str_c(file_name, '.do')), encoding = 'latin1')
+  stata_vars <- str_subset(stata, '^label variable')
+  stata_vals <- str_subset(stata, '^\\*?label define')
+  
+  var_labels <- str_match(stata_vars, 'label variable (\\S+)\\s+"([^"]+)"')
+  val_labels <- str_match(stata_vals, '(\\*)?label define label_(\\S+) (\\S+) "([^"]+)"')
+  
+  list(var_labels = as.data.frame(var_labels, stringsAsFactors = F), val_labels = as.data.frame(val_labels, stringsAsFactors = F))
+}
+
+# Function to add variable labels
+add_var_labels <- function(df, var_labels) {
+  for (i in 1:nrow(var_labels)) {
+    n <- var_labels[[i, 2]]  # col name
+    l <- var_labels[[i, 3]]  # col label
+    
+    var_label(df[[n]]) <- l  # add var label
+  }
+  
+  df
+}
+
+# Function to add value labels
+add_val_labels <- function(df, val_labels) {
+  for (i in seq_len(nrow(val_labels))) {
+    n <- val_labels[[i, 3]]  # col name
+    v <- val_labels[[i, 4]]  # val name
+    l <- val_labels[[i, 5]]  # val label
+    
+    if (is.na(n)) next
+    
+    if (is.na(val_labels[[i, 2]]) && !is.character(df[[n]])) {
+      v <- as.numeric(v)
+    }
+
+    val_label(df[[n]], v) <- l  # add val label
+  }
+  
+  df
+}
+
+# Function to append data from multiple years
+get_panel_data <- function(years, file_prefix, file_suffix = '') {
+  all_data <- vector(mode = 'list', length = length(years))
+  all_var_labels <- vector(mode = 'list', length = length(years))
+  all_val_labels <- vector(mode = 'list', length = length(years))
+  
+  for (y in 1:length(years)) { 
+    # Read in CSV data
+    file_name <- str_c(file_prefix, years[[y]], file_suffix)
+    all_data[[y]] <- csv_to_df(csv_dir, file_name, years[[y]])
+    
+    # Read in stata file labels
+    stata_labels <- get_stata_labels(stata_dir, file_name)
+    all_var_labels[[y]] <- stata_labels$var_labels
+    all_val_labels[[y]] <- stata_labels$val_labels
+  }
+  
+  # https://stackoverflow.com/a/29419402/6373540
+  combined_data <- dplyr::bind_rows(all_data)
+  combined_var_labels <- dplyr::bind_rows(all_var_labels) %>% unique()
+  combined_val_labels <- dplyr::bind_rows(all_val_labels) %>% unique()
+
+  combined_data %>% add_var_labels(combined_var_labels) %>% add_val_labels(combined_val_labels)
+}
+
+
+# hd2015 <- get_panel_data(years = '2015', file_prefix = 'hd')
+hd <- get_panel_data(years = years, file_prefix = 'hd')
+var_label(hd)
+val_labels(hd)
+save(hd, file = file.path(output_dir, 'hd_panel_data.RData'))
+
+flags <- get_panel_data(years = years, file_prefix = 'flags')
+var_label(flags)
+val_labels(flags)
+save(flags, file = file.path(output_dir, 'flags_panel_data.RData'))
+
+ic <- get_panel_data(years = years, file_prefix = 'ic')
+var_label(ic)
+val_labels(ic)
+save(ic, file = file.path(output_dir, 'ic_panel_data.RData'))
+
+ic_ay <- get_panel_data(years = years, file_prefix = 'ic', file_suffix = '_ay')
+var_label(ic_ay)
+val_labels(ic_ay)
+save(ic_ay, file = file.path(output_dir, 'ic_ay_panel_data.RData'))
+
+panel_data <- hd %>%
+  left_join(flags, by = c('year', 'unitid')) %>% 
+  left_join(ic, by = c('year', 'unitid')) %>% 
+  left_join(ic_ay, by = c('year', 'unitid'))
+save(panel_data, file = file.path(output_dir, 'panel_data.RData'))
 
 ## -----------------------------------------------------------------------------
 ## END SCRIPT
